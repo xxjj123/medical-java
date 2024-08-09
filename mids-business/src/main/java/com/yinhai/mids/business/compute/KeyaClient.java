07615dad0594f2cab3f19533204d3aa47d28e5a7
zhuhs <hs.zhu@foxmail.com>
2024-08-09 17:33:20 +0800

    优化

--- mids-business/src/main/java/com/yinhai/mids/business/compute/KeyaClient.java
+++ mids-business/src/main/java/com/yinhai/mids/business/compute/KeyaClient.java
@@ -10,11 +10,11 @@ import java.io.InputStream;
  */
 public interface KeyaClient {
 
-    @Post(url = "http://183.247.165.2:2480/connector/api/common/from/register/pacs", dataType = "json")
+    @Post(url = "{url}", dataType = "json")
     KeyaResponse register(@Var("url") String url,
                           @DataFile(value = "files", fileName = "dicom") InputStream in, @Body RegisterParam other);
 
-    @Get(url = "http://183.247.165.2:2480/connector/api/common/result", dataType = "json", readTimeout = 30000)
+    @Get(url = "{url}", dataType = "json", readTimeout = 30000)
     KeyaResponse result(@Var("url") String url, @Query("applyId") String applyId);
 
 }
