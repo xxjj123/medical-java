0ea2d2479d76157b64d38321ab5171da33363a3b
zhuhs <hs.zhu@foxmail.com>
2024-10-17 17:54:56 +0800

    任务锁优化

--- mids-business/src/main/java/com/yinhai/mids/business/service/impl/TaskLockServiceImpl.java
+++ mids-business/src/main/java/com/yinhai/mids/business/service/impl/TaskLockServiceImpl.java
@@ -1,13 +1,14 @@
 package com.yinhai.mids.business.service.impl;
 
 import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.yinhai.mids.business.constant.TaskType;
 import com.yinhai.mids.business.entity.po.TaskLockPO;
 import com.yinhai.mids.business.mapper.TaskLockMapper;
 import com.yinhai.mids.business.service.TaskLockService;
-import com.yinhai.mids.business.constant.TaskType;
 import com.yinhai.mids.common.util.DbClock;
 import com.yinhai.ta404.core.transaction.annotation.TaTransactional;
 import org.springframework.stereotype.Service;
@@ -25,42 +26,46 @@ import java.util.Date;
 @TaTransactional
 public class TaskLockServiceImpl implements TaskLockService {
 
+    private static final Log log = LogFactory.get();
+
     @Resource
     private TaskLockMapper taskLockMapper;
 
     @Override
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public boolean tryLock(TaskType taskType, String itemId, int expireSeconds) {
-        TaskLockPO taskLockPO = taskLockMapper.selectOne(Wrappers.<TaskLockPO>lambdaQuery()
-                .eq(TaskLockPO::getTaskType, taskType.getType())
-                .eq(TaskLockPO::getItemId, itemId));
-        if (taskLockPO == null) {
-            taskLockMapper.createLock(IdUtil.getSnowflakeNextIdStr(), itemId, taskType.getType());
-            taskLockPO = taskLockMapper.selectOne(Wrappers.<TaskLockPO>lambdaQuery()
+        try {
+            TaskLockPO taskLockPO = taskLockMapper.selectOne(Wrappers.<TaskLockPO>lambdaQuery()
                     .eq(TaskLockPO::getTaskType, taskType.getType())
                     .eq(TaskLockPO::getItemId, itemId));
-        }
-        Date now = DbClock.now();
+            Date now = DbClock.now();
+            Date expireTime = expireSeconds <= 0 ? null : DateUtil.offsetSecond(now, expireSeconds);
+            if (taskLockPO == null) {
+                String id = IdUtil.getSnowflakeNextIdStr();
+                int created = taskLockMapper.createLock(id, itemId, taskType.getType(), expireTime);
+                return created > 0;
+            }
 
-        // 已锁住并且未失效
-        if (BooleanUtil.isTrue(taskLockPO.getEffective())
-            && (taskLockPO.getExpireTime() == null || taskLockPO.getExpireTime().after(now))) {
+            // 不会失效或者尚未失效
+            if (taskLockPO.getExpireTime() == null || taskLockPO.getExpireTime().after(now)) {
+                return false;
+            }
+
+            int updated = taskLockMapper.update(new TaskLockPO(), Wrappers.<TaskLockPO>lambdaUpdate()
+                    .eq(TaskLockPO::getId, taskLockPO.getId())
+                    .set(TaskLockPO::getExpireTime, expireTime));
+            return updated > 0;
+        } catch (Exception e) {
+            log.error("tryLock exception occurred: {}", e.getClass().getName());
             return false;
         }
-
-        taskLockPO.setEffective(true);
-        taskLockPO.setEffectiveTime(now);
-        taskLockPO.setExpireTime(expireSeconds <= 0 ? null : DateUtil.offsetSecond(now, expireSeconds));
-        int updated = taskLockMapper.updateById(taskLockPO);
-        return updated > 0;
     }
 
     @Override
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public void unlock(TaskType taskType, String itemId) {
-        taskLockMapper.update(new TaskLockPO(), Wrappers.<TaskLockPO>lambdaUpdate()
+        taskLockMapper.delete(Wrappers.<TaskLockPO>lambdaQuery()
                 .eq(TaskLockPO::getTaskType, taskType.getType())
-                .eq(TaskLockPO::getItemId, itemId)
-                .set(TaskLockPO::getEffective, false));
+                .eq(TaskLockPO::getItemId, itemId));
     }
 }
